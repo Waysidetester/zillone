a771f4e82338920084210dc7479fcb12205627b6
WaysideTester <dylanmurray211@outlook.com>
2018-12-11 19:36:17 -0600

    init firebase

---
+++ src/components/auth/auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div className="Auth">
+        <button className="btn btn-primary">Login</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
