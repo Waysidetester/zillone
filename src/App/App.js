7ed1ed447665c4460752a5d14b98d21d27412754
WaysideTester <dylanmurray211@outlook.com>
2018-12-11 18:09:33 -0600

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
+    console.log(this);
     return (
       <div className="App">
         <header className="App-header">
