cc41e7192970aba5c2219a92e41d5d945d956462
WaysideTester <dylanmurray211@outlook.com>
2018-12-15 10:39:16 -0600

    shaped listing object for propType

--- src/components/listings/listings.js
+++ src/components/listings/listings.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import listingShape from '../../helpers/props/listingShape';
 import './listings.scss';
 
 class Listings extends React.Component {
+  static propTypes = {
+    listings: PropTypes.arrayOf(listingShape),
+  }
+
   render() {
     return (
       <div className="listings col">
