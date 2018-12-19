8c2d821cb3da3ff1535d6368077b00f9d2635c47
WaysideTester <dylanmurray211@outlook.com>
2018-12-18 19:20:46 -0600

    added form elements

--- src/components/addListing/addListing.js
+++ src/components/addListing/addListing.js
@@ -6,6 +6,18 @@ class AddListing extends React.Component {
     return (
       <div className="add-listing col">
         <h2>Add Listing</h2>
+        <form>
+          <div className="form-group">
+            <label for="address">Address:</label>
+            <input
+            type="text"
+            className="form-control"
+            id="address"
+            placeholder="123 Main St. City, State ZIPCODE"
+            />
+          </div>
+          <button className="btn btn-primary">Save Listing</button>
+        </form>
       </div>
     );
   }
